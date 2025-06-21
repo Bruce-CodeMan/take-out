eafb0a82bf9571c9bc929dc4fbe7ca8e26cf7066
Bruce-CodeMan <1033684650@qq.com>
2025-06-21 11:02:44 +0800

    :sparkles: feat: transfer the datetime using JacksonFormatter

--- take-out-api/src/main/java/com/brucecompiler/config/WebMvcConfiguration.java
+++ take-out-api/src/main/java/com/brucecompiler/config/WebMvcConfiguration.java
@@ -1,10 +1,15 @@
 package com.brucecompiler.config;
 
 import com.brucecompiler.interceptor.JwtTokenAdminInterceptor;
+import com.brucecompiler.json.JacksonObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
@@ -21,5 +26,19 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/admin/employee/login");
+    }
+
+    /**
+     * 扩展mvc框架的消息转换器
+     */
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建一个消息转换器对象
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+
+        // 设置对象转换器, 可以将Java对象转为json字符串
+        converter.setObjectMapper(new JacksonObjectMapper());
+
+        // 将我们自己的转换器放入spring MVC框架中
+        converters.add(0, converter);
     }
 }
--- take-out-pojo/src/main/java/com/brucecompiler/entity/Employee.java
+++ take-out-pojo/src/main/java/com/brucecompiler/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.brucecompiler.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
